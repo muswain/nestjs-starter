700dc5adb74f741709bd44291f75f68447769239
muswain <mrutyunjay.swain@gmail.com>
2023-04-02 11:10:19 +0800

    feat: :sparkles: add global interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logger/logger.interceptor';
 
 async function bootstrap() {
   console.info('🚀 Bootstarting Local Server 🚀');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   console.info('🚀 Bootstarting appmodule🚀');
   const globalPrefix = 'graphql';
+
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
+
   console.info(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
